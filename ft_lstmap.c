2c2324ea391767ffb91cf47f1b70a3d01f311b57
A.M.S <99796232+outsiderice@users.noreply.github.com>
2023-09-30 11:47:49 +0200

    finished lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -3,30 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstmap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amagnell <amagnell@student.42barcel>       +#+  +:+       +#+        */
+/*   By: amagnell <amagnell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 19:20:21 by amagnell          #+#    #+#             */
-/*   Updated: 2023/09/25 19:44:07 by amagnell         ###   ########.fr       */
+/*   Updated: 2023/09/30 11:44:21 by amagnell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list *ft_lstmap(t_list *lst, void *(*f) (void *), void (*del) (void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f) (void *), void (*del) (void *))
 {
 	t_list	*new_lst;
+	t_list	*new_node;
+	void	*content;
 
+	new_lst = NULL;
 	while (lst != NULL)
 	{
 		content = f(lst->content);
-		new_lst = ft_lstnew(content);
-		if(!new_lst)
+		new_node = ft_lstnew(content);
+		if (!new_node)
 		{
-			del(new_lst);
-			free(new_lst);
-			return(NULL);
+			ft_lstdelone(new_node, del);
+			free(new_node);
+			return (NULL);
 		}
+		ft_lstadd_back(&new_lst, new_node);
 		lst = lst->next;
 	}
-	return (*new_lst);
+	return (new_lst);
 }
