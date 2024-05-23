e27d459114312480563eecb326e9fec31b29e455
A.M.S <99796232+outsiderice@users.noreply.github.com>
2024-05-23 17:05:24 +0200

    lstsize and lstaddfront

--- ft_lstadd_front_bonus.c
+++ ft_lstadd_front_bonus.c
@@ -3,11 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstadd_front_bonus.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amagnell <amagnell@student.42barcel>       +#+  +:+       +#+        */
+/*   By: amagnell <amagnell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 18:10:07 by amagnell          #+#    #+#             */
-/*   Updated: 2024/05/17 18:10:14 by amagnell         ###   ########.fr       */
+/*   Updated: 2024/05/23 17:03:08 by amagnell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	new->next = *lst;
+	*lst = new;
+}
--- ft_lstsize_bonus.c
+++ ft_lstsize_bonus.c
@@ -3,11 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstsize_bonus.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amagnell <amagnell@student.42barcel>       +#+  +:+       +#+        */
+/*   By: amagnell <amagnell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 18:09:15 by amagnell          #+#    #+#             */
-/*   Updated: 2024/05/17 18:09:17 by amagnell         ###   ########.fr       */
+/*   Updated: 2024/05/23 17:03:21 by amagnell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+int	ft_lstsize(t_list *lst)
+{
+	int	i;
+
+	if (lst)
+	{
+		i = 1;
+		while (lst->next != NULL)
+		{
+			i++;
+			lst = lst->next;
+		}
+		return (i);
+	}
+	return (0);
+}
