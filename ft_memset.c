2e3679d38f75254cd29a83556f22eb3e365a3e41
A.M.S <99796232+outsiderice@users.noreply.github.com>
2024-06-10 08:57:30 +0000

    comment of memset

--- ft_memset.c
+++ ft_memset.c
@@ -3,15 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amagnell <amagnell@student.42barcel>       +#+  +:+       +#+        */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/16 17:59:58 by amagnell          #+#    #+#             */
-/*   Updated: 2022/10/08 20:14:05 by amagnell         ###   ########.fr       */
+/*   Updated: 2024/06/10 08:56:46 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+//Fills the first len bytes of b with char c.
+//Returns a pointer to b.
 void	*ft_memset(void *b, int c, size_t len)
 {
 	size_t			i;
