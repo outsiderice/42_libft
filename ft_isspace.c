ce1796431c423a335c4cf3c9a10f3dfe6f6f0bc2
A.M.S <99796232+outsiderice@users.noreply.github.com>
2024-07-27 19:15:41 +0000

    added ft_isspace

---
+++ ft_isspace.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isspace.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/27 19:04:59 by codespace         #+#    #+#             */
+/*   Updated: 2024/07/27 19:15:05 by codespace        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+// Checks whether int c is a space or not
+// Returns 0 on success and 1 on failure
+int	ft_isspace(int c)
+{
+	if ((c >= 9 && c <= 13) || c == ' ')
+		return (EXIT_SUCCESS);
+	return (EXIT_FAILURE);
+}
